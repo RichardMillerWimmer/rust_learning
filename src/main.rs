a6109cd357079352dc661ed19481d981fdca0183
Richard Miller <richardmmill@gmail.com>
2024-04-24 20:16:59 -0400

    first get endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use actix_web::{get, patch, post, HttpResponse, Responder};
+
+#[get("/pizzas")]
+async fn get_pizzas() -> impl Responder {
+    HttpResponse::Ok().body("Pizzas Available!")
+}
+
 fn main() {
     println!("Hello, world!");
 }
