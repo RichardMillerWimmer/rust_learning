80ea29a1d04beb547a71a6e13ef87c5834c88f8b
Richard Miller <richardmmill@gmail.com>
2024-05-11 10:00:56 -0400

    add endpoints

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,26 @@ async fn get_pizzas() -> impl Responder {
     HttpResponse::Ok().body("Pizzas Available!")
 }
 
+#[post("/buypizza")]
+async fn buy_pizza() -> impl Responder {
+    HttpResponse::Ok().body("Buying Pizza!")
+}
+
+#[patch("/updatepizza/{id}")]
+async fn update_pizza() -> impl Responder {
+    HttpResponse::Ok().body("Pizza Updated!")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Server!");
-    HttpServer::new(|| App::new().service(get_pizzas))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(get_pizzas)
+            .service(buy_pizza)
+            .service(update_pizza)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
