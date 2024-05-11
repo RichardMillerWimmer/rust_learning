c69c453753d14d4c9701461988d0fdd2b60e14e5
Richard Miller <richardmmill@gmail.com>
2024-05-11 09:30:41 -0400

    add servier to main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use actix_web::{get, patch, post, HttpResponse, Responder};
+use actix_web::{get, patch, post, App, HttpResponse, HttpServer, Responder};
 
 #[get("/pizzas")]
 async fn get_pizzas() -> impl Responder {
     HttpResponse::Ok().body("Pizzas Available!")
 }
 
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Server!");
+    HttpServer::new(|| App::new().service(get_pizzas))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
